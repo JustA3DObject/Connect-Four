400f3523f8f19e5907911d5ff5116b4692ab9488
JustA3DObject <81830617+JustA3DObject@users.noreply.github.com>
2022-06-14 20:32:00 +0530

    Update stylingscript.js

--- stylingscript.js
+++ stylingscript.js
@@ -1,12 +1,12 @@
 // Function to add hovering effect to columns
-const x = document.getElementById('table');
-const td = document.getElementsByTagName('td');
-const column = x.getElementsByTagName('col');
-const columnEnter = (i) => column[i].classList.add('hover');
-const columnLeave = (i) => column[i].classList.remove('hover');
+let x = document.getElementById("table");
+let td = document.getElementsByTagName("td");
+let column = x.getElementsByTagName("col");
+let columnEnter = (i) => column[i].classList.add("hover");
+let columnLeave = (i) => column[i].classList.remove("hover");
 
-for (var i = 0; i < td.length; i++) {
-  const cellIndex = td[i].cellIndex;
-  td[i].addEventListener('mouseenter', columnEnter.bind(this, cellIndex));
-  td[i].addEventListener('mouseleave', columnLeave.bind(this, cellIndex));
+for (let i = 0; i < td.length; i++) {
+    let cellIndex = td[i].cellIndex;
+    td[i].addEventListener("mouseenter", columnEnter.bind(this, cellIndex));
+    td[i].addEventListener("mouseleave", columnLeave.bind(this, cellIndex));
 }
